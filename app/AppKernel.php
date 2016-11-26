d16f686d769a7c61f5289d129f209e4e0f4b9ec2
Michał Mleczko <kontakt@michalmleczko.waw.pl>
2016-11-26 10:04:17 +0100

    Add required vendors

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new ProductBundle\ProductBundle()
+            new ProductBundle\ProductBundle(),
+            new \FOS\ElasticaBundle\FOSElasticaBundle(),
+            new \JMS\SerializerBundle\JMSSerializerBundle($this),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
